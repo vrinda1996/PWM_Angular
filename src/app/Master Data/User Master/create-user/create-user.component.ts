53d8b82431584f489b2130fd28ab13aa3d64d6df
Akash Jagwani <455654@FutureGroup.net>
2019-03-27 20:28:03 +0530

    changes

--- src/app/Master Data/User Master/create-user/create-user.component.ts
+++ src/app/Master Data/User Master/create-user/create-user.component.ts
@@ -7,6 +7,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export interface UserRole {
   name: string;
 }
+export interface HoSiteCode {
+  name: string;
+}
+export interface StoreSiteCode {
+  name: string;
+}
+export interface ZoneSiteCode {
+  name: string;
+}
+export interface FormatList {
+  name: string;
+}
+export interface DescriptionList {
+  name: string;
+}
 
 @Component({
   selector: 'app-create-user',
@@ -16,6 +31,7 @@ export interface UserRole {
 export class CreateUserComponent implements OnInit {
 
   employeeForm = new FormGroup({
+    existingEmployeeName: new FormControl(''),
     employeeCode: new FormControl(''),
     employeeName: new FormControl(''),
     contactNumber: new FormControl(''),
@@ -31,27 +47,73 @@ export class CreateUserComponent implements OnInit {
     location: new FormControl({ value: 'location', disabled: true}),
     zone: new FormControl({ value: 'zone', disabled: true}),
   });
- 
-  existingEmployeeNameForm = new FormGroup({
-    existingEmployeeName: new FormControl(''),
-  });
-  userRoleControl= new FormControl('', [Validators.required]);
-  public userRoles = [];
-
   constructor(private http:HttpClient) { }
 
   ngOnInit() {
     this.getUserRoleList();
+    this.getHoSiteCode();
+    this.getStoreSiteCode();
+    this.getFormatList();
+    this.getZoneSiteCode();
+    this.getDescriptionList();
   }
 
   onSubmit() {
   }
+  userRoleControl= new FormControl('', [Validators.required]);
+  public userRoles = [];
   private userRoleUrl = 'http://localhost:8082/roleAPI/getRole';
-
   public getUserRoleList() {
-    console.log("res");
+    console.log("user");
     return this.http.get<UserRole[]>(this.userRoleUrl).subscribe((res : any[])=>{
       this.userRoles = res;
+    });
+  }
+  
+  hoSiteCodeControl= new FormControl('', [Validators.required]);
+  public hoSiteCode = [];
+  private HoSiteCodeUrl = 'http://localhost:8082/roleAPI/getRole';
+  public getHoSiteCode() {
+    console.log("Ho");
+    return this.http.get<HoSiteCode[]>(this.HoSiteCodeUrl).subscribe((res : any[])=>{
+      this.hoSiteCode = res;
+    });
+  }
+
+  storeSiteCodeControl= new FormControl('', [Validators.required]);
+  public storeSiteCode = [];
+  private StoreSiteCodeUrl = 'http://localhost:8082/roleAPI/getRole';
+  public getStoreSiteCode() {
+    console.log("Store");
+    return this.http.get<StoreSiteCode[]>(this.StoreSiteCodeUrl).subscribe((res : any[])=>{
+      this.storeSiteCode = res;
+    });
+  }
+  formatControl= new FormControl('', [Validators.required]);
+  public formatList = [];
+  private FormatUrl = 'http://localhost:8082/roleAPI/getRole';
+  public getFormatList() {
+    console.log("format");
+    return this.http.get<FormatList[]>(this.FormatUrl).subscribe((res : any[])=>{
+      this.formatList = res;
+    });
+  }
+  zoneSiteCodeControl= new FormControl('', [Validators.required]);
+  public zoneSiteCode = [];
+  private ZoneSiteCodeUrl = 'http://localhost:8082/roleAPI/getRole';
+  public getZoneSiteCode() {
+    console.log("Zone");
+    return this.http.get<ZoneSiteCode[]>(this.ZoneSiteCodeUrl).subscribe((res : any[])=>{
+      this.zoneSiteCode = res;
+    });
+  }
+  descriptionControl= new FormControl('', [Validators.required]);
+  public descriptionList = [];
+  private DescriptionListUrl = 'http://localhost:8082/roleAPI/getRole';
+  public getDescriptionList() {
+    console.log("Description");
+    return this.http.get<DescriptionList[]>(this.DescriptionListUrl).subscribe((res : any[])=>{
+      this.descriptionList = res;
     });
   }
 }
